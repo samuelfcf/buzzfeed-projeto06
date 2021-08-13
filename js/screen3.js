0193d3f1974127af9b1c59ea7ebe6cafe6e4b1b6
Samuel Felipe <samuelfelipef@gmail.com>
2021-08-13 15:53:38 -0300

    add get all questions info feature

--- js/screen3.js
+++ js/screen3.js
@@ -1,5 +1,10 @@
-let quizz = {};
-let numQuestionsRender;
+let quizz = {
+    title: "title",
+    image: "urlImage",
+    questions: new Array(3),
+    levels: new Array(2)
+}
+let numQuestionsRender=2;
 let numLevelsRender;
 
 // first screen
@@ -7,6 +12,11 @@ function validateURL(url) {
     const rule = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
     return rule.test(url)
 }
+
+function isHexCodeColor(color) {
+    const rule = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/i;
+    return rule.test(color);
+}  
 
 function saveQuizzInfoBasics() {
     let title = document.querySelector(".quizz-title").value;
@@ -79,4 +89,75 @@ function next() {
     document.querySelector(".screen3-3").classList.remove("hidden");
 }
 
-// ultima função feita é a de rnederizar niveis.
+function questionInfo(question) {
+    const qTitle = document.querySelector(`#q${question}-text`).value;
+    const qColor = document.querySelector(`#q${question}-color`).value;
+
+    const qCorrectAnswer = document.querySelector(`#q${question}-correct-answer`).value;
+    const qCorrectAnswerURLImage = document.querySelector(`#q${question}-urlimage-correct-answer`).value;
+
+    const qIncorrectAnswer1 = document.querySelector(`#q${question}-incorrect-answer1`).value;
+    const qIncorrectAnswer1_URLImage = document.querySelector(`#q${question}-urlimage-incorrect-answer1`).value;
+
+    const qIncorrectAnswer2 = document.querySelector(`#q${question}-incorrect-answer2`).value;
+    const qIncorrectAnswer2_URLImage = document.querySelector(`#q${question}-urlimage-incorrect-answer2`).value;
+
+    const qIncorrectAnswer3 = document.querySelector(`#q${question}-incorrect-answer3`).value;
+    const qIncorrectAnswer3_URLImage = document.querySelector(`#q${question}-urlimage-incorrect-answer3`).value;
+
+    let answers = [];
+
+    if( (qTitle.length < 20) || 
+        (isHexCodeColor(qColor) === false) || 
+        (qCorrectAnswer === '' || qIncorrectAnswer1 === '' || qIncorrectAnswer2 === '' || qIncorrectAnswer3 === '') ||
+        (validateURL(qCorrectAnswerURLImage) === false || validateURL(qIncorrectAnswer1_URLImage) === false || validateURL(qIncorrectAnswer2_URLImage) === false || validateURL(qIncorrectAnswer3_URLImage) === false) )
+        {
+            alert("falhou!!!");
+        }
+     
+    else {
+        const answer1 = {
+            text: qCorrectAnswer,
+            image: qCorrectAnswerURLImage,
+            isCorrectAnswer: true
+        }
+    
+        const answer2 = {
+            text: qIncorrectAnswer1,
+            image: qIncorrectAnswer1_URLImage,
+            isCorrectAnswer: false
+        }
+    
+        const answer3 = {
+            text: qIncorrectAnswer2,
+            image: qIncorrectAnswer2_URLImage,
+            isCorrectAnswer: false
+        }
+    
+        const answer4 = {
+            text: qIncorrectAnswer3,
+            image: qIncorrectAnswer3_URLImage,
+            isCorrectAnswer: false
+        }
+    
+        answers.push(answer1, answer2, answer3, answer4);
+    
+        const objQuestion = {
+            title: qTitle,
+            color: qColor,
+            answers: answers,
+        }
+    
+        quizz.questions.push(objQuestion);
+    }    
+    
+    
+}
+
+function getAllQuestionsInfo() {
+    for(let i=0; i<numQuestionsRender; i++) {
+        questionInfo(i+1);
+        console.log(quizz)
+    }
+}
+
